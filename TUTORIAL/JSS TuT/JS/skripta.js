51183f7b13de17dc73798d33e5608f87502b9f18
DedicE <brothers17@live.com>
2021-11-02 23:59:08 +0100

    tutorial

---
+++ TUTORIAL/JSS TuT/JS/skripta.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".text-primary").addClass("animated shake");
+
+    animated shake animated bounce btn-primary"
+    $("body").addClass("animated fadeOut");
+});

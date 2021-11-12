a4f302326e8df2905703b9715bf17040747080ea
DedicE <brothers17@live.com>
2021-11-12 10:21:48 +0100

    sortiranje

--- TUTORIAL/JSS TuT/JS/skripta.js
+++ TUTORIAL/JSS TuT/JS/skripta.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $("button").addClass("animated bounce");
     $(".text-primary").addClass("animated shake");
 
-    animated shake animated bounce btn-primary"
+    animated shake animated bounce btn - primary "
     $("body").addClass("animated fadeOut");
 });
